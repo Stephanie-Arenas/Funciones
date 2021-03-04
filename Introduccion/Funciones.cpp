f9c4b87529adeafc09c828bfe616b9f5a5094690
Stephanie-Arenas <79618697+Stephanie-Arenas@users.noreply.github.com>
2021-03-03 21:40:23 -0600

    Add files via upload

---
+++ Introduccion/Funciones.cpp
@@ -0,0 +1,26 @@
+//ARENAS LOPEZ NADIA STEPHANIE 2AV4
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+void resta(int a, int b);
+
+int main ()
+{
+    int a, b, c;
+    cout<<"Introduzca el primer valor: " << endl;
+    cin>>a;
+    cout<<"Introduzca el segundo valor: " <<endl;
+    cin>>b;
+
+void resta(int a, int b);
+    c= a-b;
+    cout<<"El resultado de la resta es: " << c <<endl;
+    return 0;
+
+
+}
